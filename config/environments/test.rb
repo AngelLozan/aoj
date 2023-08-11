54bef7532b6b96ffebc7cb523aab68f9a9b2184e
Scott Lozano <scottloz@protonmail.com>
2023-08-11 22:01:44 +0100

    updates active storage and frontend

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
   config.action_controller.allow_forgery_protection = false
 
   # Store uploaded files on the local file system in a temporary directory.
+  config.active_storage.replace_on_assign_to_many = false
   config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
