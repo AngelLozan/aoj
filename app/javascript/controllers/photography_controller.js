203c34955a58b8b84b62e9bb8db0702842a2cb4e
Scott Lozano <scottloz@protonmail.com>
2023-08-22 16:08:17 +0100

    updates cv format

--- app/javascript/controllers/photography_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-// import Lightbox from 'stimulus-lightbox'
-
-// Connects to data-controller="photography"
-export default class extends Controller {
-  connect() {
-    console.log("connected photography controller");
-  }
-}
