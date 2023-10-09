d5ba77db223a865ce63d520832046a34c50c3ab5
Scott Lozano <scottloz@protonmail.com>
2023-10-09 15:55:26 +0100

    updates order controller logic

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[new create]
   before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :load_cart
 
   def index
     @orders = Order.all
@@ -9,22 +10,31 @@ class OrdersController < ApplicationController
   def new
     @order = Order.new
     @cart.each do |painting|
-      @order.paintings << painting
+      @order.order_paintings << painting
     end
   end
 
   def create
-
+    @order = Order.new(order_params)
+    # TODO: Add logic to set order status to "open" and create order
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 
   def edit
   end
-  
+
   def update
+    respond_to do |format|
+      if @order.update(order_params)
+        format.html { redirect_to admin_url, notice: "Order was successfully updated." }
+        format.json { render :show, status: :ok, location: @order }
+      else
+        format.html { render :admin, status: :unprocessable_entity }
+        format.json { render json: @order.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -35,9 +45,25 @@ class OrdersController < ApplicationController
     end
   end
 
+  def load_cart
+    if session[:cart].nil?
+      session[:cart] = []
+      @cart = session[:cart]
+    else
+      @cart = Painting.find(session[:cart])
+    end
+  end
+
   private
+
+  def order_params
+    # order_paintings: [] is an array of painting ids to set from the cart before save
+    params.require(:order).permit(:address, :phone, :status, order_paintings:[])
+  end
 
   def set_order
     @order = Order.find(params[:id])
   end
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
+  has_many :order_paintings
   validates :address, presence: true
   validates :phone, presence: true
   enum :status, { open: 0, complete: 1 }
