84acd6deb21ba24a0dcd7e7de3936eb058055003
Scott Lozano <scottloz@protonmail.com>
2023-10-12 17:54:22 +0100

    Create order with paintings from cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,5 @@
 class Order < ApplicationRecord
-  has_many_attached :paintings
+  has_many :paintings
   validates :address, presence: true
   validates :phone, presence: true
   enum :status, { open: 0, complete: 1 }
