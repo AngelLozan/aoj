fde450ea37190c0df2ff9d5f47b807c07baca63d
Scott Lozano <scottloz@protonmail.com>
2024-01-30 12:19:23 +0000

    updates seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,8 @@ let_the_church_say = Painting.create!({ title: "Let the church say", description
   file_1 = File.open(file_path_1)
   let_the_church_say.photos.attach(io: file_1, filename: "let_the_church_say.jpeg", content_type: "image/jpeg")
   file_1.close
+puts "Created #{let_the_church_say.title}"
+
 four_generations = Painting.create!({ title: "Four generations", price: 120000, description:
   """
   Original painting by Jaleh Sadravi
@@ -41,6 +43,8 @@ four_generations = Painting.create!({ title: "Four generations", price: 120000, 
   file_2 = File.open(file_path_2)
   four_generations.photos.attach(io: file_2, filename: "four_generations.jpeg", content_type: "image/jpeg")
   file_2.close
+puts "Created #{four_generations.title}"
+
 kindred_souls = Painting.create!({ title: "Kindred souls", price: 62500, description:
  """
   Original painting by Jaleh Sadravi
@@ -55,6 +59,8 @@ kindred_souls = Painting.create!({ title: "Kindred souls", price: 62500, descrip
   file_3 = File.open(file_path_3)
   kindred_souls.photos.attach(io: file_3, filename: "kindred_souls.jpeg", content_type: "image/jpeg")
   file_3.close
+puts "Created #{kindred_souls.title}"
+
 community_of_faith = Painting.create!({ title: "Community of faith", price: 142500, description:
   """
   Original painting by Jaleh Sadravi
@@ -67,6 +73,8 @@ community_of_faith = Painting.create!({ title: "Community of faith", price: 1425
   file_4 = File.open(file_path_4)
   community_of_faith.photos.attach(io: file_4, filename: "community_of_faith.jpeg", content_type: "image/jpeg")
   file_4.close
+puts "Created #{community_of_faith.title}"
+
 free_to_shine = Painting.create!({ title: "Free to shine", price: 160000, description:
   """
   Original painting by Jaleh Sadravi
@@ -79,7 +87,9 @@ free_to_shine = Painting.create!({ title: "Free to shine", price: 160000, descri
   file_5 = File.open(file_path_5)
   free_to_shine.photos.attach(io: file_5, filename: "free_to_shine.jpeg", content_type: "image/jpeg")
   file_5.close
-we_call_their_name_so_we_remember = Painting.create!({ title: "We call their name so we remember", price: 240000, description:
+puts "Created #{free_to_shine.title}"
+
+we_call_their_names_so_we_remember = Painting.create!({ title: "We call their names so we remember", price: 240000, description:
   """
   Original painting by Jaleh Sadravi
 
@@ -87,10 +97,12 @@ we_call_their_name_so_we_remember = Painting.create!({ title: "We call their nam
 
   """
 })
-  file_path_6 = Rails.root.join("app", "assets", "images", "we_call_their_name_so_we_remember.jpeg")
+  file_path_6 = Rails.root.join("app", "assets", "images", "we_call_their_names_so_we_remember.jpeg")
   file_6 = File.open(file_path_6)
-  we_call_their_name_so_we_remember.photos.attach(io: file_6, filename: "we_call_their_name_so_we_remember.jpeg", content_type: "image/jpeg")
+  we_call_their_name_so_we_remember.photos.attach(io: file_6, filename: "we_call_their_names_so_we_remember.jpeg", content_type: "image/jpeg")
   file_6.close
+puts "Created #{we_call_their_name_so_we_remember.title}"
+
 our_family_is_our_strength = Painting.create!({ title: "Our family is our strength", price: 114500, description:
   """
   Original painting by Jaleh Sadravi
@@ -105,6 +117,8 @@ our_family_is_our_strength = Painting.create!({ title: "Our family is our streng
   file_7 = File.open(file_path_7)
   our_family_is_our_strength.photos.attach(io: file_7, filename: "our_family_is_our_strength.jpeg", content_type: "image/jpeg")
   file_7.close
+puts "Created #{our_family_is_our_strength.title}"
+
 family_is_love = Painting.create!({ title: "Family is love", price: 112000, description:
   """
   Original painting by Jaleh Sadravi
@@ -119,6 +133,8 @@ family_is_love = Painting.create!({ title: "Family is love", price: 112000, desc
   file_8 = File.open(file_path_8)
   family_is_love.photos.attach(io: file_8, filename: "family_is_love.jpeg", content_type: "image/jpeg")
   file_8.close
+puts "Created #{family_is_love.title}"
+
 following_the_spirit = Painting.create!({ title: "Following the spirit", price: 152500, description:
   """
   Original painting by Jaleh Sadravi
@@ -131,6 +147,8 @@ following_the_spirit = Painting.create!({ title: "Following the spirit", price: 
   file_9 = File.open(file_path_9)
   following_the_spirit.photos.attach(io: file_9, filename: "following_the_spirit.jpeg", content_type: "image/jpeg")
   file_9.close
+puts "Created #{following_the_spirit.title}"
+
 in_the_shelter_of_your_arms = Painting.create!({ title: "In the shelter of your arms", price: 125000, description:
   """
   Original canvas painting by Jaleh Sadravi
@@ -145,6 +163,8 @@ in_the_shelter_of_your_arms = Painting.create!({ title: "In the shelter of your 
   file_10 = File.open(file_path_10)
   in_the_shelter_of_your_arms.photos.attach(io: file_10, filename: "in_the_shelter_of_your_arms.jpeg", content_type: "image/jpeg")
   file_10.close
+puts "Created #{in_the_shelter_of_your_arms.title}"
+
 passing_down_our_wisdom = Painting.create!({ title: "Passing down our wisdom, expanding our roots", price: 182500, description:
   """
   Original painting by Jaleh Sadravi
@@ -159,6 +179,8 @@ passing_down_our_wisdom = Painting.create!({ title: "Passing down our wisdom, ex
   file_11 = File.open(file_path_11)
   passing_down_our_wisdom.photos.attach(io: file_11, filename: "passing_down_our_wisdom.jpeg", content_type: "image/jpeg")
   file_11.close
+puts "Created #{passing_down_our_wisdom.title}"
+
 gathering_of_women = Painting.create!({ title: "Gathering of women", price: 261000, description:
   """
   Original painting by Jaleh Sadravi
@@ -175,6 +197,7 @@ gathering_of_women = Painting.create!({ title: "Gathering of women", price: 2610
   file_12 = File.open(file_path_12)
   passing_down_our_wisdom.photos.attach(io: file_12, filename: "passing_down_our_wisdom.jpeg", content_type: "image/jpeg")
   file_12.close
+puts "Created #{gathering_of_women.title}"
 
 
 
