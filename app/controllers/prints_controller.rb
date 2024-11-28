66d9dce33aec2ac64e75c47341ab568c0eeeccec
Scott Lozano <scottloz@protonmail.com>
2024-11-28 13:34:05 +0000

    Adds admin panel for prints/shop, unpublish button and refactor

--- app/controllers/prints_controller.rb
+++ app/controllers/prints_controller.rb
@@ -13,71 +13,7 @@ class PrintsController < ApplicationController
   before_action :load_orders
 
   def index
-
-    @products = Rails.cache.fetch("products", expires_in: 1.hour) do
-      shop_id = ENV['PRINTIFY_SHOP_ID']
-      url = URI("https://api.printify.com/v1/shops/#{shop_id}/products.json");
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true;
-
-      request = Net::HTTP::Get.new(url)
-      request["Authorization"] = "Bearer #{ENV['PRINTIFY']}"
-      request["Content-Type"] = "application/json"
-      request["Accept"] = "application/json"
-      request["User-Agent"] = "RUBY"
-      request["Cache-Control"] = "private, max-age=3600, no-revalidate"
-      # request.body = JSON.dump({}) # if you need to send a body with the request...
-
-      response = http.request(request)
-      # products = response.read_body
-      raw_data = JSON.parse(response.read_body)
-
-      products = raw_data["data"]
-
-      products = products.map do |product|
-        p "=========================================="
-        p "Product is: #{product["id"]}"
-        p "=========================================="
-
-
-        parsed_description = Nokogiri::HTML.parse(product['description'])
-
-        default_variant = product['variants'].find { |variant| variant['is_default'] == true && variant['is_enabled'] == true}
-
-
-        if default_variant
-          price = default_variant['price']
-          variant = default_variant['id']
-        else
-          first_variant = product['variants'].first
-          price = first_variant['price']
-          variant = first_variant['id']
-        end
-
-
-        if product["images"].empty?
-          {
-            'id' => product['id'],
-            'title' => product['title'],
-            'description' => parsed_description.text,
-            'image' => 'abstractart.png',
-            'price' => price, # product['variants'].first['price'],
-            'variant' => variant # product['variants'].first['id']
-          }
-        else
-          {
-            'id' => product['id'],
-            'title' => product['title'],
-            'description' => parsed_description.text,
-            'image' => product["images"].first["src"],
-            'price' => price,
-            'variant' => variant
-          }
-        end
-
-      end
-    end
-    @products = @products
+    @products = load_products
     if !params[:query] || params[:query].empty?
       @products = Kaminari.paginate_array(@products).page(params[:page]).per(10)
     end
@@ -91,6 +27,10 @@ class PrintsController < ApplicationController
     @products = @products.select {|p| p["title"].downcase.include?(params[:query].downcase)}
     @products = Kaminari.paginate_array(@products).page(params[:page]).per(10)
 
+  end
+
+  def prints_admin
+    @products = load_products
   end
 
   def show
@@ -175,6 +115,51 @@ class PrintsController < ApplicationController
     end
   end
 
+  def unpublish_print
+    begin
+      id = sanitize_id(params[:id])
+      shop_id = ENV['PRINTIFY_SHOP_ID']
+      url = URI("https://api.printify.com/v1/shops/#{shop_id}/products/#{id}/publishing_failed.json");
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true;
+      request = Net::HTTP::Post.new(url)
+      request["Authorization"] = "Bearer #{ENV['PRINTIFY']}"
+      request["Content-Type"] = "application/json"
+      request["Accept"] = "application/json"
+      request["User-Agent"] = "RUBY"
+      # request.body = JSON.dump({
+      #   external: {
+      #     id: "#{id}",
+      #     handle: "https://theartofjaleh.com/prints/show/#{id}"
+      #   }
+      # })
+      request.body = JSON.dump({
+        reason: "I need to remove this print from my shop."
+      })
+
+      response = http.request(request)
+      raw_data = JSON.parse(response.read_body)
+      Rails.logger.info("==========================================")
+      Rails.logger.info("Response is: #{raw_data}")
+      Rails.logger.info("==========================================")
+
+      if raw_data = "#{id}"
+        flash[:notice] = "Print has been unpublished 👍, please remove it from your shop in the printify console."
+      else
+        flash[:notice] = "Could not unpublish the print 🚨, please try again or contact support."
+      end
+
+      redirect_to prints_admin_path
+    rescue StandardError => e
+      Rails.logger.error("==========================================")
+      Rails.logger.error("Error is: #{e}")
+      Rails.logger.error("==========================================")
+      flash[:notice] = "Could not unpublish the print 🚨, please try again.
+      Error: #{e}"
+      redirect_to print_admin_paths
+    end
+  end
+
   def publish_print
     begin
       id = sanitize_id(params[:id])
@@ -219,6 +204,73 @@ class PrintsController < ApplicationController
   end
 
   private
+
+  def load_products
+    @products = Rails.cache.fetch("products", expires_in: 1.hour) do
+      shop_id = ENV['PRINTIFY_SHOP_ID']
+      url = URI("https://api.printify.com/v1/shops/#{shop_id}/products.json");
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true;
+
+      request = Net::HTTP::Get.new(url)
+      request["Authorization"] = "Bearer #{ENV['PRINTIFY']}"
+      request["Content-Type"] = "application/json"
+      request["Accept"] = "application/json"
+      request["User-Agent"] = "RUBY"
+      request["Cache-Control"] = "private, max-age=3600, no-revalidate"
+      # request.body = JSON.dump({}) # if you need to send a body with the request...
+
+      response = http.request(request)
+      # products = response.read_body
+      raw_data = JSON.parse(response.read_body)
+
+      products = raw_data["data"]
+
+      products = products.map do |product|
+        p "=========================================="
+        p "Product is: #{product["id"]}"
+        p "=========================================="
+
+
+        parsed_description = Nokogiri::HTML.parse(product['description'])
+
+        default_variant = product['variants'].find { |variant| variant['is_default'] == true && variant['is_enabled'] == true}
+
+
+        if default_variant
+          price = default_variant['price']
+          variant = default_variant['id']
+        else
+          first_variant = product['variants'].first
+          price = first_variant['price']
+          variant = first_variant['id']
+        end
+
+
+        if product["images"].empty?
+          {
+            'id' => product['id'],
+            'title' => product['title'],
+            'description' => parsed_description.text,
+            'image' => 'abstractart.png',
+            'price' => price, # product['variants'].first['price'],
+            'variant' => variant # product['variants'].first['id']
+          }
+        else
+          {
+            'id' => product['id'],
+            'title' => product['title'],
+            'description' => parsed_description.text,
+            'image' => product["images"].first["src"],
+            'price' => price,
+            'variant' => variant
+          }
+        end
+
+      end
+    end
+    @products = @products
+  end
 
   def load_cart
     if session[:cart].nil?
